ee7beb13dd06a85227a71c07681be5fdceed52d5
abbas <153435280+Null-Studio@users.noreply.github.com>
2023-12-10 17:44:32 +0330

    main form
    this is main form after login

---
+++ dashboard.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace LiberaryManegment.Properties
+{
+    partial class dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(dashboard));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.کتابهاToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.افزودنکتابToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.مشاهده_کتابهاToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.دانشجویانToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.افزودندانشجوToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.مشاهدهدانشجویانToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.بخشامانتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.برگرداندنکتابToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.اطلاعاتکاملToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.خروجToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تغییرفونتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تغییر_رمز_عبور_نام_کاربریToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تغییررنگToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.خروجToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Wheat;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.کتابهاToolStripMenuItem,
+            this.دانشجویانToolStripMenuItem,
+            this.بخشامانتToolStripMenuItem,
+            this.برگرداندنکتابToolStripMenuItem,
+            this.اطلاعاتکاملToolStripMenuItem,
+            this.خروجToolStripMenuItem,
+            this.خروجToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip1.Size = new System.Drawing.Size(1347, 58);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // کتابهاToolStripMenuItem
+            // 
+            this.کتابهاToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.افزودنکتابToolStripMenuItem,
+            this.مشاهده_کتابهاToolStripMenuItem});
+            this.کتابهاToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("کتابهاToolStripMenuItem.Image")));
+            this.کتابهاToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.کتابهاToolStripMenuItem.Name = "کتابهاToolStripMenuItem";
+            this.کتابهاToolStripMenuItem.Size = new System.Drawing.Size(107, 54);
+            this.کتابهاToolStripMenuItem.Text = "کتاب ها";
+            // 
+            // افزودنکتابToolStripMenuItem
+            // 
+            this.افزودنکتابToolStripMenuItem.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.افزودنکتابToolStripMenuItem.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_add_book_48;
+            this.افزودنکتابToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.افزودنکتابToolStripMenuItem.Name = "افزودنکتابToolStripMenuItem";
+            this.افزودنکتابToolStripMenuItem.Size = new System.Drawing.Size(189, 56);
+            this.افزودنکتابToolStripMenuItem.Text = "افزودن کتاب";
+            this.افزودنکتابToolStripMenuItem.Click += new System.EventHandler(this.افزودنکتابToolStripMenuItem_Click);
+            // 
+            // مشاهده_کتابهاToolStripMenuItem
+            // 
+            this.مشاهده_کتابهاToolStripMenuItem.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.مشاهده_کتابهاToolStripMenuItem.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_view_50;
+            this.مشاهده_کتابهاToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.مشاهده_کتابهاToolStripMenuItem.Name = "مشاهده_کتابهاToolStripMenuItem";
+            this.مشاهده_کتابهاToolStripMenuItem.Size = new System.Drawing.Size(189, 56);
+            this.مشاهده_کتابهاToolStripMenuItem.Text = "مشاهده کتاب ها";
+            this.مشاهده_کتابهاToolStripMenuItem.Click += new System.EventHandler(this.مشاهده_کتابهاToolStripMenuItem_Click);
+            // 
+            // دانشجویانToolStripMenuItem
+            // 
+            this.دانشجویانToolStripMenuItem.BackColor = System.Drawing.Color.Orange;
+            this.دانشجویانToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.افزودندانشجوToolStripMenuItem,
+            this.مشاهدهدانشجویانToolStripMenuItem});
+            this.دانشجویانToolStripMenuItem.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_student_male_50;
+            this.دانشجویانToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.دانشجویانToolStripMenuItem.Name = "دانشجویانToolStripMenuItem";
+            this.دانشجویانToolStripMenuItem.Size = new System.Drawing.Size(121, 54);
+            this.دانشجویانToolStripMenuItem.Text = "دانشجویان";
+            // 
+            // افزودندانشجوToolStripMenuItem
+            // 
+            this.افزودندانشجوToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.افزودندانشجوToolStripMenuItem.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_add_user_male_50;
+            this.افزودندانشجوToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.افزودندانشجوToolStripMenuItem.Name = "افزودندانشجوToolStripMenuItem";
+            this.افزودندانشجوToolStripMenuItem.Size = new System.Drawing.Size(203, 56);
+            this.افزودندانشجوToolStripMenuItem.Text = "افزودن دانشجو";
+            this.افزودندانشجوToolStripMenuItem.Click += new System.EventHandler(this.افزودندانشجوToolStripMenuItem_Click);
+            // 
+            // مشاهدهدانشجویانToolStripMenuItem
+            // 
+            this.مشاهدهدانشجویانToolStripMenuItem.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.مشاهدهدانشجویانToolStripMenuItem.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_view_50;
+            this.مشاهدهدانشجویانToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.مشاهدهدانشجویانToolStripMenuItem.Name = "مشاهدهدانشجویانToolStripMenuItem";
+            this.مشاهدهدانشجویانToolStripMenuItem.Size = new System.Drawing.Size(203, 56);
+            this.مشاهدهدانشجویانToolStripMenuItem.Text = "مشاهده دانشجویان";
+            this.مشاهدهدانشجویانToolStripMenuItem.Click += new System.EventHandler(this.مشاهدهدانشجویانToolStripMenuItem_Click);
+            // 
+            // بخشامانتToolStripMenuItem
+            // 
+            this.بخشامانتToolStripMenuItem.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_book_philosophy_50;
+            this.بخشامانتToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.بخشامانتToolStripMenuItem.Name = "بخشامانتToolStripMenuItem";
+            this.بخشامانتToolStripMenuItem.Size = new System.Drawing.Size(125, 54);
+            this.بخشامانتToolStripMenuItem.Text = "بخش امانت";
+            this.بخشامانتToolStripMenuItem.Click += new System.EventHandler(this.بخشامانتToolStripMenuItem_Click);
+            // 
+            // برگرداندنکتابToolStripMenuItem
+            // 
+            this.برگرداندنکتابToolStripMenuItem.BackColor = System.Drawing.Color.OldLace;
+            this.برگرداندنکتابToolStripMenuItem.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_return_book_50;
+            this.برگرداندنکتابToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.برگرداندنکتابToolStripMenuItem.Name = "برگرداندنکتابToolStripMenuItem";
+            this.برگرداندنکتابToolStripMenuItem.Size = new System.Drawing.Size(141, 54);
+            this.برگرداندنکتابToolStripMenuItem.Text = "برگرداندن کتاب";
+            this.برگرداندنکتابToolStripMenuItem.Click += new System.EventHandler(this.برگرداندنکتابToolStripMenuItem_Click);
+            // 
+            // اطلاعاتکاملToolStripMenuItem
+            // 
+            this.اطلاعاتکاملToolStripMenuItem.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_books_50;
+            this.اطلاعاتکاملToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.اطلاعاتکاملToolStripMenuItem.Name = "اطلاعاتکاملToolStripMenuItem";
+            this.اطلاعاتکاملToolStripMenuItem.Size = new System.Drawing.Size(181, 54);
+            this.اطلاعاتکاملToolStripMenuItem.Text = "اطلاعات کامل کتاب ها ";
+            this.اطلاعاتکاملToolStripMenuItem.Click += new System.EventHandler(this.اطلاعاتکاملToolStripMenuItem_Click);
+            // 
+            // خروجToolStripMenuItem
+            // 
+            this.خروجToolStripMenuItem.BackColor = System.Drawing.Color.Cornsilk;
+            this.خروجToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.تغییرفونتToolStripMenuItem,
+            this.تغییر_رمز_عبور_نام_کاربریToolStripMenuItem,
+            this.تغییررنگToolStripMenuItem});
+            this.خروجToolStripMenuItem.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_setting_48;
+            this.خروجToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.خروجToolStripMenuItem.Name = "خروجToolStripMenuItem";
+            this.خروجToolStripMenuItem.Size = new System.Drawing.Size(110, 54);
+            this.خروجToolStripMenuItem.Text = "تنظیمات";
+            // 
+            // تغییرفونتToolStripMenuItem
+            // 
+            this.تغییرفونتToolStripMenuItem.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_font_48;
+            this.تغییرفونتToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.تغییرفونتToolStripMenuItem.Name = "تغییرفونتToolStripMenuItem";
+            this.تغییرفونتToolStripMenuItem.Size = new System.Drawing.Size(234, 54);
+            this.تغییرفونتToolStripMenuItem.Text = "تغییر فونت";
+            this.تغییرفونتToolStripMenuItem.Click += new System.EventHandler(this.تغییرفونتToolStripMenuItem_Click);
+            // 
+            // تغییر_رمز_عبور_نام_کاربریToolStripMenuItem
+            // 
+            this.تغییر_رمز_عبور_نام_کاربریToolStripMenuItem.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_password_40;
+            this.تغییر_رمز_عبور_نام_کاربریToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.تغییر_رمز_عبور_نام_کاربریToolStripMenuItem.Name = "تغییر_رمز_عبور_نام_کاربریToolStripMenuItem";
+            this.تغییر_رمز_عبور_نام_کاربریToolStripMenuItem.Size = new System.Drawing.Size(234, 54);
+            this.تغییر_رمز_عبور_نام_کاربریToolStripMenuItem.Text = "تغییر رمز عبور و نام کاربری";
+            this.تغییر_رمز_عبور_نام_کاربریToolStripMenuItem.Click += new System.EventHandler(this.تغییر_رمز_عبور_نام_کاربریToolStripMenuItem_Click);
+            // 
+            // تغییررنگToolStripMenuItem
+            // 
+            this.تغییررنگToolStripMenuItem.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_rgb_color_wheel_48;
+            this.تغییررنگToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.تغییررنگToolStripMenuItem.Name = "تغییررنگToolStripMenuItem";
+            this.تغییررنگToolStripMenuItem.Size = new System.Drawing.Size(234, 54);
+            this.تغییررنگToolStripMenuItem.Text = "تغییر رنگ";
+            this.تغییررنگToolStripMenuItem.Click += new System.EventHandler(this.تغییررنگToolStripMenuItem_Click);
+            // 
+            // خروجToolStripMenuItem1
+            // 
+            this.خروجToolStripMenuItem1.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_exit_sign_50;
+            this.خروجToolStripMenuItem1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.خروجToolStripMenuItem1.Name = "خروجToolStripMenuItem1";
+            this.خروجToolStripMenuItem1.Size = new System.Drawing.Size(94, 54);
+            this.خروجToolStripMenuItem1.Text = "خروج";
+            this.خروجToolStripMenuItem1.Click += new System.EventHandler(this.خروجToolStripMenuItem1_Click);
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fontDialog1.ShowApply = true;
+            this.fontDialog1.ShowColor = true;
+            // 
+            // dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1347, 667);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "dashboard";
+            this.Text = "dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.dashboard_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem کتابهاToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem افزودنکتابToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem مشاهده_کتابهاToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem دانشجویانToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem افزودندانشجوToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem مشاهدهدانشجویانToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem بخشامانتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem برگرداندنکتابToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem اطلاعاتکاملToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem خروجToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem خروجToolStripMenuItem1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ToolStripMenuItem تغییرفونتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تغییر_رمز_عبور_نام_کاربریToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تغییررنگToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
---
+++ dashboard.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows.Forms;
+namespace LiberaryManegment.Properties
+{
+    public partial class dashboard : Form
+    {
+     
+        public dashboard()
+        {
+            InitializeComponent();
+
+        }
+        private void افزودنکتابToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddBook ab = new AddBook();
+            ab.Show();
+        }
+
+        private void مشاهده_کتابهاToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            viewbook vb = new viewbook();
+            vb.Show();
+        }
+
+        private void افزودندانشجوToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddStudent AS = new AddStudent();
+            AS.Show();
+        }
+
+        private void مشاهدهدانشجویانToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            viewStudent vs = new viewStudent();
+            vs.Show();
+        }
+
+        private void بخشامانتToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BookIseeu bi = new BookIseeu();
+            bi.Show();
+        }
+
+        private void برگرداندنکتابToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bookreturn br = new bookreturn();
+            br.Show();
+        }
+
+        private void اطلاعاتکاملToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            completbook cb = new completbook();
+            cb.Show();
+        }
+
+        private void خروجToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(" مطمئن هستید می خواهید خارج شوید ؟", "توجه", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+      
+
+        private void تغییر_رمز_عبور_نام_کاربریToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Change_Pass_Username ch = new Change_Pass_Username();
+            ch.ShowDialog();
+        }
+
+        private void تغییرفونتToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(fontDialog1.ShowDialog()==DialogResult.OK)
+            {
+                menuStrip1.Font = fontDialog1.Font;
+                Properties.Settings.Default.font = fontDialog1.Font;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void dashboard_Load(object sender, EventArgs e)
+        {
+            Font = Properties.Settings.Default.font;
+            ForeColor = Properties.Settings.Default.color;
+        }
+
+        private void تغییررنگToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           if(colorDialog1.ShowDialog()==DialogResult.OK)
+            {
+                Properties.Settings.Default.color = colorDialog1.Color;
+                Properties.Settings.Default.Save();
+                menuStrip1.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void پشتیبان_گیریToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
