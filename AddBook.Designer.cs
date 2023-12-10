4fa1c3b7fabb39daea70f430fb75c9d2192ab16b
abbas <153435280+Null-Studio@users.noreply.github.com>
2023-12-10 17:45:59 +0330

    add book from
    form for add books

---
+++ AddBook.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace LiberaryManegment.Properties
+{
+    partial class AddBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.cancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.save = new System.Windows.Forms.Button();
+            this.textbookprice = new System.Windows.Forms.TextBox();
+            this.textbookname = new System.Windows.Forms.TextBox();
+            this.textbooktype = new System.Windows.Forms.TextBox();
+            this.textpobliesher = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textwhriter = new System.Windows.Forms.TextBox();
+            this.poblishdata1 = new System.Windows.Forms.DateTimePicker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Wheat;
+            this.panel1.BackgroundImage = global::LiberaryManegment.Properties.Properties.Resources.OIG__6_;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(683, 92);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Wheat;
+            this.label3.Cursor = System.Windows.Forms.Cursors.No;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Location = new System.Drawing.Point(360, 41);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(86, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "افزودن کتاب";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LiberaryManegment.Properties.Properties.Resources.OIG;
+            this.pictureBox2.Location = new System.Drawing.Point(1, 90);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(284, 336);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel2.BackgroundImage = global::LiberaryManegment.Properties.Properties.Resources.OIG__6_;
+            this.panel2.Controls.Add(this.cancel);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.save);
+            this.panel2.Controls.Add(this.textbookprice);
+            this.panel2.Controls.Add(this.textbookname);
+            this.panel2.Controls.Add(this.textbooktype);
+            this.panel2.Controls.Add(this.textpobliesher);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.textwhriter);
+            this.panel2.Controls.Add(this.poblishdata1);
+            this.panel2.Location = new System.Drawing.Point(282, 90);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(402, 336);
+            this.panel2.TabIndex = 1;
+            // 
+            // cancel
+            // 
+            this.cancel.BackColor = System.Drawing.Color.Snow;
+            this.cancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cancel.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.cancel.Location = new System.Drawing.Point(88, 287);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 16;
+            this.cancel.Text = "لغو";
+            this.cancel.UseVisualStyleBackColor = false;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(296, 200);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(73, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "قیمت کتاب :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(297, 242);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label5.Size = new System.Drawing.Size(72, 19);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "دسته بندی :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(290, 155);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(84, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "تاریخ انتشار :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(290, 116);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(79, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "منتشر کننده :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.Color.Snow;
+            this.save.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.save.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.save.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.save.Location = new System.Drawing.Point(204, 287);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 15;
+            this.save.Text = "ذخیره";
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // textbookprice
+            // 
+            this.textbookprice.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textbookprice.Location = new System.Drawing.Point(79, 199);
+            this.textbookprice.Name = "textbookprice";
+            this.textbookprice.Size = new System.Drawing.Size(200, 22);
+            this.textbookprice.TabIndex = 14;
+            this.textbookprice.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textbookprice.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textbookprice_KeyPress);
+            // 
+            // textbookname
+            // 
+            this.textbookname.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textbookname.Location = new System.Drawing.Point(79, 25);
+            this.textbookname.Name = "textbookname";
+            this.textbookname.Size = new System.Drawing.Size(200, 22);
+            this.textbookname.TabIndex = 13;
+            this.textbookname.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textbooktype
+            // 
+            this.textbooktype.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textbooktype.Location = new System.Drawing.Point(79, 242);
+            this.textbooktype.Name = "textbooktype";
+            this.textbooktype.Size = new System.Drawing.Size(200, 22);
+            this.textbooktype.TabIndex = 12;
+            this.textbooktype.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textpobliesher
+            // 
+            this.textpobliesher.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textpobliesher.Location = new System.Drawing.Point(79, 115);
+            this.textpobliesher.Name = "textpobliesher";
+            this.textpobliesher.Size = new System.Drawing.Size(200, 22);
+            this.textpobliesher.TabIndex = 11;
+            this.textpobliesher.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(299, 75);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(58, 19);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "نویسنده :";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(297, 26);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label7.Size = new System.Drawing.Size(61, 19);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "نام کتاب :";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textwhriter
+            // 
+            this.textwhriter.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textwhriter.Location = new System.Drawing.Point(79, 74);
+            this.textwhriter.Name = "textwhriter";
+            this.textwhriter.Size = new System.Drawing.Size(200, 22);
+            this.textwhriter.TabIndex = 10;
+            this.textwhriter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // poblishdata1
+            // 
+            this.poblishdata1.Location = new System.Drawing.Point(79, 155);
+            this.poblishdata1.Name = "poblishdata1";
+            this.poblishdata1.Size = new System.Drawing.Size(200, 20);
+            this.poblishdata1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImage = global::LiberaryManegment.Properties.Properties.Resources.OIG__6_;
+            this.pictureBox1.Image = global::LiberaryManegment.Properties.Properties.Resources.add_book;
+            this.pictureBox1.Location = new System.Drawing.Point(261, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 92);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AddBook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::LiberaryManegment.Properties.Properties.Resources.OIG__6_;
+            this.ClientSize = new System.Drawing.Size(684, 428);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "AddBook";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "افزودن کتاب";
+            this.Load += new System.EventHandler(this.AddBook_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.TextBox textbookprice;
+        private System.Windows.Forms.TextBox textbookname;
+        private System.Windows.Forms.TextBox textbooktype;
+        private System.Windows.Forms.TextBox textpobliesher;
+        private System.Windows.Forms.TextBox textwhriter;
+        private System.Windows.Forms.DateTimePicker poblishdata1;
+    }
+}
---
+++ AddBook.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LiberaryManegment.Properties
+{
+    public partial class AddBook : Form
+    {
+        public AddBook()
+        {
+            InitializeComponent();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (textbookname.Text != "" && textwhriter.Text != "" && textpobliesher.Text != "" && textbookprice.Text != "" && textbooktype.Text != "")
+            {
+                string bname = textbookname.Text;
+                string bauthor = textwhriter.Text;
+                string publish = textpobliesher.Text;
+                string pdata = poblishdata1.Text;
+                string price = textbookprice.Text;
+                string type = textbooktype.Text;
+
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "Data Source=.;Initial Catalog=library;Integrated Security=True";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+
+
+                con.Open();
+                cmd.CommandText = "insert into NewBook1 (bName,bAuthor,bPubl,bPDate,bPrice,type)values(N'" + bname + "',N'" + bauthor + "',N'" + publish + "',N'" + pdata + "',N'" + price + "',N'" + type + "')";
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("با موفقیت ذخیره شد", "موفق", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textbookname.Clear();
+                textwhriter.Clear();
+                textpobliesher.Clear();
+                textbookprice.Clear();
+                textbooktype.Clear();
+            }
+            else
+            {
+                MessageBox.Show("!نمی توانید فیلد ها را خالی بگذارید", "!توجه", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(" این عمل باعث از دست رفتن اطلاعات ذخیره نشده میشود", "مطمِئن هستید؟", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            {
+                this.Close();
+            }
+        }
+
+        private void textbookprice_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("لطفا مقدار عددی وارد کنید", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void AddBook_Load(object sender, EventArgs e)
+        {
+            Font = Properties.Settings.Default.font;
+            ForeColor = Properties.Settings.Default.color;
+        }
+    }
+  
+}
