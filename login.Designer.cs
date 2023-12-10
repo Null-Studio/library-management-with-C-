f6f956b017e807b2795f0a6a753505a4d3be3f60
abbas <153435280+Null-Studio@users.noreply.github.com>
2023-12-10 17:39:09 +0330

    login form
    this login form

---
+++ login.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textusername = new System.Windows.Forms.TextBox();
+            this.textpassword = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.close = new System.Windows.Forms.PictureBox();
+            this.telegram = new System.Windows.Forms.PictureBox();
+            this.instagram = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.telegram)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.instagram)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Cyan;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(146, 445);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ورود";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textusername
+            // 
+            this.textusername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(36)))), ((int)(((byte)(55)))));
+            this.textusername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textusername.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textusername.ForeColor = System.Drawing.Color.White;
+            this.textusername.Location = new System.Drawing.Point(120, 298);
+            this.textusername.Margin = new System.Windows.Forms.Padding(4);
+            this.textusername.Name = "textusername";
+            this.textusername.Size = new System.Drawing.Size(212, 20);
+            this.textusername.TabIndex = 3;
+            this.textusername.Text = "نام کاربری";
+            this.textusername.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textusername_MouseClick);
+            // 
+            // textpassword
+            // 
+            this.textpassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(36)))), ((int)(((byte)(55)))));
+            this.textpassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textpassword.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textpassword.ForeColor = System.Drawing.Color.White;
+            this.textpassword.Location = new System.Drawing.Point(120, 380);
+            this.textpassword.Margin = new System.Windows.Forms.Padding(4);
+            this.textpassword.Name = "textpassword";
+            this.textpassword.Size = new System.Drawing.Size(212, 20);
+            this.textpassword.TabIndex = 4;
+            this.textpassword.Text = "رمز عبور";
+            this.textpassword.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textpassword_MouseClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(120, 322);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(213, 2);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(119, 405);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(213, 2);
+            this.panel2.TabIndex = 10;
+            // 
+            // close
+            // 
+            this.close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.close.Image = ((System.Drawing.Image)(resources.GetObject("close.Image")));
+            this.close.Location = new System.Drawing.Point(3, 4);
+            this.close.Margin = new System.Windows.Forms.Padding(4);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(33, 28);
+            this.close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.close.TabIndex = 11;
+            this.close.TabStop = false;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // telegram
+            // 
+            this.telegram.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.telegram.Image = ((System.Drawing.Image)(resources.GetObject("telegram.Image")));
+            this.telegram.Location = new System.Drawing.Point(120, 501);
+            this.telegram.Margin = new System.Windows.Forms.Padding(4);
+            this.telegram.Name = "telegram";
+            this.telegram.Size = new System.Drawing.Size(60, 52);
+            this.telegram.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.telegram.TabIndex = 8;
+            this.telegram.TabStop = false;
+            this.telegram.Click += new System.EventHandler(this.telegram_Click);
+            // 
+            // instagram
+            // 
+            this.instagram.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.instagram.Image = ((System.Drawing.Image)(resources.GetObject("instagram.Image")));
+            this.instagram.Location = new System.Drawing.Point(272, 501);
+            this.instagram.Margin = new System.Windows.Forms.Padding(4);
+            this.instagram.Name = "instagram";
+            this.instagram.Size = new System.Drawing.Size(60, 52);
+            this.instagram.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.instagram.TabIndex = 7;
+            this.instagram.TabStop = false;
+            this.instagram.Click += new System.EventHandler(this.instagram_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(340, 357);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(60, 48);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(340, 283);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(60, 52);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LiberaryManegment.Properties.Properties.Resources.khoshnevisan1;
+            this.pictureBox1.Location = new System.Drawing.Point(136, 85);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(163, 137);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(185, 208);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 50);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImage = global::LiberaryManegment.Properties.Properties.Resources.color;
+            this.pictureBox6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox6.Image = global::LiberaryManegment.Properties.Properties.Resources.icons8_info;
+            this.pictureBox6.Location = new System.Drawing.Point(408, 4);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(33, 28);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 13;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(36)))), ((int)(((byte)(55)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(444, 567);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.telegram);
+            this.Controls.Add(this.instagram);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textpassword);
+            this.Controls.Add(this.textusername);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "login";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.telegram)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.instagram)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textusername;
+        private System.Windows.Forms.TextBox textpassword;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox instagram;
+        private System.Windows.Forms.PictureBox telegram;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox close;
+        private System.Windows.Forms.PictureBox pictureBox6;
+    }
+}
+
---
+++ login.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using LiberaryManegment.Properties;
+
+namespace WindowsFormsApplication1
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dashboard dsh = new dashboard();
+            if (textusername.Text == LiberaryManegment.Properties.Properties.Settings.Default.username && textpassword.Text == LiberaryManegment.Properties.Properties.Settings.Default.password)
+            {
+                dsh.Show();
+                this.Hide();
+                MessageBox.Show(" (نرم افزار مدیریت کتابخانه دانشگاه فنی و حرفه ای گلپایگان(شهدا", "خوش آمدید", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("! رمز عبور یا نام کاربری اشتباه است", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+               
+            }
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void telegram_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://t.me/Largepotatoes");
+        }
+
+        private void instagram_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://instagram.com/memes._angel?igshid=MzRIODBiNWFIZA==");
+        }
+
+        private void textusername_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textusername.Text == "نام کاربری")
+                textusername.Clear();
+        }
+
+        private void textpassword_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textpassword.Text == "رمز عبور")
+                textpassword.Clear();
+            textpassword.PasswordChar = '*';
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            info inf = new info();
+            Hide();
+            inf.Show();
+        }
+    }
+}
